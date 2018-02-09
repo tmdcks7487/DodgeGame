b609c57adcead8c2817095888c48cba1e0cc19a3
Seungchan Lim <tmdcks7487@naver.com>
2018-02-09 17:02:55 +0900

    upload

---
+++ Assets/Scripts/disappearenemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class disappearenemy : MonoBehaviour {
+    private float starttime;
+    public float timeover = 5.0f;
+  
+    private void Start()
+    {
+        starttime = Time.time;
+    }
+
+    private void Update()
+    {
+        if(Time.time-starttime>timeover)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
